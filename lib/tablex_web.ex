0b208c53c4d2ed54ae7589670764c67c79d0ea35
Drew Cain <drew@simpli.fi>
2018-02-27 12:35:48 -0600

    cleanup

--- lib/tablex_web.ex
+++ lib/tablex_web.ex
@@ -28,8 +28,9 @@ defmodule TablexWeb do
 
   def view do
     quote do
-      use Phoenix.View, root: "lib/tablex_web/templates",
-                        namespace: TablexWeb
+      use Phoenix.View,
+        root: "lib/tablex_web/templates",
+        namespace: TablexWeb
 
       # Import convenience functions from controllers
       import Phoenix.Controller, only: [get_flash: 2, view_module: 1]
